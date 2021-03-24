a7364fed8fb4923704c5f7e5143b8bdbdd174ff1
flin <flin@adobe.com>
2021-03-24 14:27:59 -0700

    Introduction

---
+++ UltimatePortfolio/UltimatePortfolio/UltimatePortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  UltimatePortfolioApp.swift
+//  UltimatePortfolio
+//
+//  Created by Felix Lin on 3/24/21.
+//
+
+import SwiftUI
+
+@main
+struct UltimatePortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
