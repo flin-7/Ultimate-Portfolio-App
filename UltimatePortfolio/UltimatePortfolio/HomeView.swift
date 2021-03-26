1538b9c27a7eaeec8327fdcefaae8ebc9c3818f4
flin-7 <flin.7@icloud.com>
2021-03-25 20:45:55 -0700

    First steps in UI

---
+++ UltimatePortfolio/UltimatePortfolio/HomeView.swift
@@ -0,0 +1,30 @@
+//
+//  HomeView.swift
+//  UltimatePortfolio
+//
+//  Created by Felix Lin on 3/25/21.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    @EnvironmentObject var dataController: DataController
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                Button("Add Data") {
+                    dataController.deleteAll()
+                    try? dataController.createSampleDate()
+                }
+            }
+            .navigationTitle("Home")
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
